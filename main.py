4fa8d687e99fc700f2b9bf86a37edb3b51d6a8ee
RamsesCamas <machiniram@gmail.com>
2021-10-17 22:57:23 -0500

    Finish first version

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python
 
-from tkinter import *
+from tkinter import Label, Tk,Button, Entry
+from tkinter import messagebox, font
 import re
-
-fechas =['20/11/2018','30/04/2021','11-06-1976','00-03-01','31/04/2011']
 
 def check_pattern():
     pattern = re.compile(r'^(([12][0-9]|0[1-9])[\/\-]02|([12][0-9]|0[1-9]|3[01])[\/\-](0[13578]|1[02])|([12][0-9]|0[1-9]|30)[\/\-](0[469]|11))[\/\-][0-9]{2,4}$')
-    for fecha in fechas:
+    fecha = text_field.get()
+    if len(fecha) != 0:
         res = re.match (pattern,fecha)
         if res:
             print(fecha)
+            messagebox.showinfo(message=f'{fecha}--- Formato válido', title='Resultado')
         else:
-            print(f'{fecha}--- Formato inválido')
+            messagebox.showinfo(message=f'{fecha}--- Formato inválido', title='Resultado')
+    else: messagebox.showerror(message='Ingrese algo',title='Error')
 
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Fechas Regex')
     root.geometry('600x300')
+    label_title = Label(root,text='Validador de fechas',width=19,height=1)
+    label_title['font'] = font.Font(size=13)
+    label_title.place(x=150,y=0)
+
+    text_field = Entry(root,width=20)
+    text_field['font'] = font.Font(size=12)
+    text_field.place(x=150,y=100)
+
+    btn_process_date = Button(root,text='Procesar',command=check_pattern)
+    btn_process_date.place(x=220,y=150)
+    btn_process_date['font'] = font.Font(size=13)
+
+
     root.mainloop()
